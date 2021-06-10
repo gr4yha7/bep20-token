ee86041de58b93a164c7d98fd09e08de3597bd84
gr4yha7 <jamaal.ekpenyong22@gmail.com>
2021-06-10 03:01:52 +0100

    testing a few things out, preventing reentrancy

--- contracts/BepToken.sol
+++ contracts/BepToken.sol
@@ -2,26 +2,38 @@
 pragma solidity >= 0.7.0 < 0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 contract Bep20Token is ERC20 {
-  string private constant name = "Beibs";
-  string private constant symbol = "BEIB";
-  
-  constructor() ERC20(name, symbol) {
+  using SafeMath for uint256;
+  using Address for address;
+
+  constructor() ERC20("Beibs", "BEIB") {
     address owner = msg.sender;
     uint totalSupply = 1e32;
     _mint(owner, totalSupply);
   }
 
   modifier isValidAddress(address addr) {
-    uint code;
+    uint size;
     assembly {
-      code := extcodesize(addr)
+      size := extcodesize(addr)
     }
-    require(code == 0); // contract address check
+    require(size == 0); // contract address check
     require(addr != address(0x0)); // zero address check
     require(addr != address(this)); // self contract address check
     _;
+  }
+
+  function refund(uint price) public payable {
+    uint256 amountPaid = msg.value;
+    address payable buyer = payable(msg.sender);
+    if (amountPaid > price) {
+      uint256 refundAmount = amountPaid.sub(price);
+      // solhint-disable-next-line
+      buyer.sendValue(refundAmount);
+    }
   }
 
 }
